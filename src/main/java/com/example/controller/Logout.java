82ea72fd1c2a6a79687a9271dbbf2461c3043673
kushalkk752 <kushalkk555@gmail.com>
2024-10-02 12:08:25 +0530

    Added The logout feature

---
+++ src/main/java/com/example/controller/Logout.java
@@ -0,0 +1,10 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Logout {
+    public void test(){
+        System.out.println("test");
+    }
+}
